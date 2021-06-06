9e8a5b7394ab2f5529134c29a4a67f605d81fee2
채희찬 <eqholic1125@gmail.com>
2021-06-07 01:55:58 +0900

    codeup 6098

--- CodeUp/6098.py
+++ CodeUp/6098.py
@@ -8,4 +8,22 @@ for i in range(10):
         count += 1
     count = 0
 
-print(checkerboard)
+x, y = 1, 1
+while(True):
+    if checkerboard[x][y]==2:       # 현 위치가 2일 경우
+        checkerboard[x][y] = 9
+        break
+    else:                           # 현 위치가 2가 아닐 경우
+        checkerboard[x][y] = 9
+        if checkerboard[x][y+1] == 1:       # 오른쪽이 1일 경우
+            if checkerboard[x+1][y] == 1:   # 아래쪽이 1일 경우
+                break
+            else:                   # 아래쪽이 0이나 2일 경우
+                x += 1
+        else:                       # 오른족이 0이나 2일 경우
+            y += 1
+
+for i in range(10):
+    for j in range(10):
+        print(checkerboard[i][j], end=" ")
+    print()
