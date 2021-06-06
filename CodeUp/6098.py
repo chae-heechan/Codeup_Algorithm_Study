e5f458cc96e8a116bb6392778ec73e9cfb431456
채희찬 <eqholic1125@gmail.com>
2021-06-06 23:49:10 +0900

    codeup 6098

--- CodeUp/6098.py
+++ CodeUp/6098.py
@@ -1,1 +1,11 @@
 # 성실한 개미
+checkerboard = [[0 for j in range(10)] for i in range (10)]
+count = 0
+for i in range(10):
+    row = map(int, input().split())
+    for point in row:
+        checkerboard[i][count] = point
+        count += 1
+    count = 0
+
+print(checkerboard)
